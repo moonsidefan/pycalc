a6f1ce61e19f8e5931dca73d79f3ef513482df1f
moonsidefan <92344168+moonsidefan@users.noreply.github.com>
2021-10-14 02:32:59 +0200

    Update pycalc.py

--- pycalc.py
+++ pycalc.py
@@ -4,10 +4,61 @@ import sys
 
 count = len(sys.argv)
 args = sys.argv
-num1 = 1000001
-num2 = 1000001
+num1 = 0
+num2 = 0
+limit = 2**64
 operator = ""
 res = 0
+
+def MainProgram():
+	num1 = int(input("Value for number one: "))
+	num2 = int(input("Value for number two: "))
+	if CheckNumbers(num1, num2):
+		print("For addition type '+', for subtraction type '-', for multiplication type '*', for devision type '/'. Type 'e' to edit numbers")
+		operator = input("Your choice: ")
+		if CheckSyntax(operator):
+			Calculate(num1, num2, operator)
+
+
+def CheckNumbers(num1, num2):
+	if abs(num1) > limit or abs(num2) > limit:
+		print("Numbers exceed the limit (2**64)")
+		return False
+	else:
+		return True
+
+def CheckSyntax(operator):
+	if operator == "e":
+		MainProgram()
+	elif operator not in ["+", "-", "*", "/"]:
+		print("Operators must be ")
+		return False
+	else:
+		return True
+
+def Calculate(num1, num2, operator):
+	if operator == "/" and num2 == 0:
+		print("Deviding by zero impossible")
+		quit()
+	elif operator == "+":
+		res = num1 + num2
+		print("The result is: " + str(res))
+		quit()
+	elif operator == "-":
+		res = num1 - num2
+		print("The result is: " + str(res))
+		quit()
+	elif operator == "*":
+		res = num1 * num2
+		print("The result is: " + str(res))
+		quit()
+	elif operator == "/":
+		res = num1 / num2
+		print("The result is: " + str(res))
+		quit()
+	else:
+		input("Unknown error. Press key to exit...")
+		quit()
 
 if count == 1:
 	print("Type -h for help")
@@ -20,38 +71,6 @@ else:
 		print("To start the program type -s")
 		quit()
 	elif args[1] == "-s":
-		while num1 < -1000000 or num1 > 1000000 or num2 < -1000000 or num2 > 1000000:
-			print("Numbers must be between -1000000 and 1000000.")
-			num1 = int(input("Value for number one: "))
-			num2 = int(input("Value for number two: "))
-		while operator not in ["+", "-", "*", "/", "c"]:
-			print("For addition type '+', for subtraction type '-', for multiplication type '*', for devision type '/'. Type 'c' to exit")
-			operator = input("Your choice: ")
-		if operator == "c":
-			print("Exiting...")
-			quit()
-		elif operator == "/" and num2 == 0:
-			print("Deviding by zero impossible")
-			quit()
-		else:
-			if operator == "+":
-				res = num1 + num2
-				print("The result is: " + str(res))
-				quit()
-			elif operator == "-":
-				res = num1 - num2
-				print("The result is: " + str(res))
-				quit()
-			elif operator == "*":
-				res = num1 * num2
-				print("The result is: " + str(res))
-				quit()
-			elif operator == "/":
-				res = num1 / num2
-				print("The result is: " + str(res))
-				quit()
-			else:
-				input("Unknown error. Press key to exit...")
-				quit()
-
+		MainProgram()
+	
 			
